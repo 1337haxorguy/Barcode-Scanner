3ce82fc4ab93a34c397727bacdb138d3ca0ea6e5
1337haxorguy <1337haxorguy1@gmail.com>
2024-06-04 02:04:24 -0400

    init

---
+++ ResaleApp/ResaleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResaleAppApp.swift
+//  ResaleApp
+//
+//  Created by Angelo Dela Fuente on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ResaleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
